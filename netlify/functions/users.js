29e004d10b70edc9c796dfd670c8c412b3df491b
Dodi Duha <romanusdd@gmail.com>
2025-10-09 09:14:00 +0700

    auth: set SHA-256 hashes for dodi123/alice123/bob123

--- netlify/functions/users.js
+++ netlify/functions/users.js
@@ -1,9 +1,10 @@
 // netlify/functions/users.js
+// Using SHA-256 hex hashes to match current passwords:
 // dodi  -> dodi123
 // alice -> alice123
 // bob   -> bob123
 export const USERS = {
-  dodi:  "$2a$10$yC59yknAOBMZbnZ4J4cZuOJdrN29J.1dcGKcZ2ccQCGhK2xGSo2Mu",
-  alice: "$2a$10$KqGwhEXxU1P26gx9sP93Ge4fXvWvNLRUjz6d46oiy7m6b/YXKk2nS",
-  bob:   "$2a$10$EkgdwqBi4u97MOCU/HKhieJGBI86v4OEvHSlbz1HoRLBLRqqm/jQq"
+  dodi:  "cfc2644cf97098d0c2fe7b8f5c366f495260f77b291f3987f02fa340e7165e1d",
+  alice: "4e40e8ffe0ee32fa53e139147ed559229a5930f89c2204706fc174beb36210b3",
+  bob:   "8d059c3640b97180dd2ee453e20d34ab0cb0f2eccbe87d01915a8e578a202b11"
 };
